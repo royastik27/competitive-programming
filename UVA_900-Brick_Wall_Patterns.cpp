642b469296d23773f3002a65e1d225a940a2cfb3
Astik Roy <76438969+royastik27@users.noreply.github.com>
2020-12-23 00:05:14 +0600

    Create UVA_900-Brick_Wall_Patterns.cpp

---
+++ UVA_900-Brick_Wall_Patterns.cpp
@@ -0,0 +1,39 @@
+/**
+* UVA 900 - Brick Wall Patters
+* The pattern is actually the Fibonacci sequence
+* AUTHOR: Eng. Astik Roy
+**/
+
+#include <iostream>
+
+using namespace std;
+
+long long int fib[51];
+
+void readyFib(){
+
+    int i;
+
+    fib[1] = 1;
+    fib[2] = 2;
+    for(i = 3; i<=50; ++i)
+        fib[i] = fib[i-2] + fib[i-1];
+}
+
+int main(){
+
+    readyFib();
+
+    int length;
+
+    while(1){
+
+        cin >> length;
+
+        if(!length) break;
+
+        cout << fib[length] << '\n';
+    }
+
+    return 0;
+}
