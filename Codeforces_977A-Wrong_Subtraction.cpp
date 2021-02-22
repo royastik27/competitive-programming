f5a7695b9fdbb6a261c0e53953dea929e56534ea
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-02-22 20:00:50 +0600

    Create Codeforces_977A-Wrong_Subtraction.cpp

---
+++ Codeforces_977A-Wrong_Subtraction.cpp
@@ -0,0 +1,31 @@
+/**
+* Codeforces Contest 977
+* Problem A - Wrong Subtraction
+* Time: 30 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k, rem;
+
+    cin >> n >> k;
+
+    while(k--)
+    {
+        rem = n % 10;
+
+        if(rem == 0)
+            n /= 10;
+        else
+            --n;
+    }
+
+    cout << n << '\n';
+
+    return 0;
+}
