dae32f1cf043fcba8e831cb9f2608dc1de50194c
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-02-25 00:18:30 +0600

    Create Codeforces_1095A-Repeating_Cipher.cpp

---
+++ Codeforces_1095A-Repeating_Cipher.cpp
@@ -0,0 +1,30 @@
+/**
+* Codeforces Problem 1095
+* Codeforces Round #529 (Div. 3)
+* Time: 15 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char encrypted[56], decrypted[11];
+    int length, i, j, a;
+
+    cin >> length >> encrypted;
+
+    for(i = 0, j = 0, a = 0; i < length; i += a)
+    {
+        decrypted[j++] = encrypted[i];
+
+        ++a;
+    }
+    decrypted[j] = '\0';
+
+    cout << decrypted << '\n';
+
+    return 0;
+}
