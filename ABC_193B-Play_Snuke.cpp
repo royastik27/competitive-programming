446717b43b619221cc1761f83c620e0cb2b943d4
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-03-01 13:50:34 +0600

    Create ABC_193B-Play_Snuke.cpp

---
+++ ABC_193B-Play_Snuke.cpp
@@ -0,0 +1,37 @@
+/**
+* Atcoder Beginner Contest 193
+* Problem B - Play Snuke
+* Time: 97 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+
+using namespace std;
+
+#define INF 1000000001
+
+int main()
+{
+    int nShops, time, yen, inStock, min;
+
+    cin >> nShops;
+
+    min = INF;
+    while(nShops--)
+    {
+        cin >> time >> yen >> inStock;
+
+        if((inStock - time) > 0 && yen < min)
+            min = yen;
+    }
+
+    if(min == INF)
+        cout << -1;
+    else
+        cout << min;
+
+    cout << '\n';
+
+    return 0;
+}
