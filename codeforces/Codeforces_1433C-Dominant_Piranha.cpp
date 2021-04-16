1843b4bccaf9d461793054f3c46040a57ef8c49d
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-04-16 21:37:01 +0600

    Create Codeforces_1433C-Dominant_Piranha.cpp

---
+++ codeforces/Codeforces_1433C-Dominant_Piranha.cpp
@@ -0,0 +1,62 @@
+/**
+* Codeforces Round #677 (Div. 3)
+* Problem C - Dominant Piranha
+* Time: 717 ms
+**/
+
+#include <iostream>
+
+using namespace std;
+
+#define SIZE 300002
+
+int main()
+{
+    int TC, nPiranhas, p_size[SIZE], max_size, max_position, count_max, prev;
+    register int i;
+
+    cin >> TC;
+
+    while(TC--)
+    {
+        cin >> nPiranhas;
+
+        max_size = count_max = 0;
+
+        for(i = 1; i <= nPiranhas; ++i)
+        {
+            cin >> p_size[i];
+            if(p_size[i] > max_size)
+                max_size = p_size[i];
+        }
+
+        p_size[0] = p_size[nPiranhas+1] = max_size;
+
+        max_size = 0;
+
+        // getting the position
+        for(i = 1; i <= nPiranhas; ++i)
+        {
+            if(p_size[i] > max_size)
+            {
+                max_size = p_size[i];
+                max_position = i;
+                count_max = 1;
+            }
+            else if(p_size[i] == max_size)
+            {
+                ++count_max;
+
+                if(p_size[i-1] != max_size || p_size[i+1] != max_size)
+                    max_position = i;
+            }
+        }
+
+        if(count_max == nPiranhas)
+            max_position = -1;
+
+        cout << max_position << '\n';
+    }
+
+    return 0;
+}
