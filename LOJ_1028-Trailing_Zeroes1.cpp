6f47c8b110bc47361948d2b2bdabc21c1ef67e3f
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-04-18 20:12:09 +0600

    Create LOJ_1028-Trailing_Zeroes1.cpp

---
+++ LOJ_1028-Trailing_Zeroes1.cpp
@@ -0,0 +1,84 @@
+/**
+* LightOJ 1028 - Trailing Zeroes (I)
+* Time: 359 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+#define PRIME_LIMIT 78504
+#define LIMIT 1000100
+
+long long int primes[PRIME_LIMIT];
+
+void seive()
+{
+    long long int i, j;
+    int root = sqrt(LIMIT), k = -1;
+    bool isPrime[LIMIT + 1];
+
+    for(i = 3; i <= LIMIT; i += 2)
+        isPrime[i] = true;
+
+    primes[++k] = 2;
+
+    for(i = 3; i <= root; i += 2)
+    {
+        if(isPrime[i])
+        {
+            for(j = i*i; j <= LIMIT; j += i + i)
+            isPrime[j] = false;
+        }
+    }
+
+    for(i = 3; i <= LIMIT; i += 2)
+    {
+        if(isPrime[i])
+            primes[++k] = i;
+    }
+
+    return;
+}
+
+int main()
+{
+    int TC, NOD, k = 0, power, root, i;
+    long long int num;
+
+    seive();
+
+    scanf("%d", &TC);
+
+    while(TC--)
+    {
+        scanf("%lld", &num);
+
+        root = sqrt(num);
+
+        NOD = 1;
+
+        for(i = 0; primes[i]*primes[i] <= num; ++i)
+        {
+            if(num % primes[i] == 0)
+            {
+                power = 0;
+                while(num % primes[i] == 0)
+                {
+                    ++power;
+                    num /= primes[i];
+                }
+                NOD *= (power+1);
+            }
+        }
+
+        // maximum one prime factor more than the square root
+        if(num > 1) NOD *= 2;
+
+        printf("Case %d: %d\n", ++k, NOD-1);
+    }
+
+    return 0;
+}
