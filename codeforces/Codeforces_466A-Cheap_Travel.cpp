7599ce788d13dfabdc3bedfae034c23cc4d4492c
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-04-24 01:02:20 +0600

    Create Codeforces_466A-Cheap_Travel.cpp

---
+++ codeforces/Codeforces_466A-Cheap_Travel.cpp
@@ -0,0 +1,54 @@
+/**
+* Codeforces Round #266 (Div. 2)
+* Problem A - Cheap Travel
+* Time: 31 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define LIMIT 150001
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int n, m, a, b, cost, special;
+
+    cin >> n >> m >> a >> b;
+
+    if(m > n)
+    {
+        special = 1;
+
+        if(special*b < n*a)
+            cost = special * b;
+        else cost = n*a;
+    }
+    else
+    {
+        special = floor((double)n / m);
+
+        if(special*b < special*m*a)
+            cost = special * b;
+        else
+            cost = special*m*a;
+
+        n -= special*m;
+
+        if(n)
+        {
+            if(b < n * a)
+                cost += b;
+            else
+                cost += n*a;
+        }
+    }
+
+    cout << cost << '\n';
+
+    return 0;
+}
