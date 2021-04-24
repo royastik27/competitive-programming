bdc7f3e1dcf1717a948ae8f6e2baba741a47c6a5
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-04-24 12:09:56 +0600

    Create Codeforces_489B-BerSU_Ball2.cpp

---
+++ codeforces/Codeforces_489B-BerSU_Ball2.cpp
@@ -0,0 +1,54 @@
+/**
+* Codeforces Round #277.5 (Div. 2)
+* Problem B - BerSU Ball
+* Iterative Solution using two pointer method
+* Time: 31 ms
+* AUTHOR: Astik Roy
+**/
+ 
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+ 
+using namespace std;
+ 
+#define LIMIT 100
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+ 
+    int nBoys, nGirls, boy[LIMIT], girl[LIMIT], res;
+    register int i, j;
+ 
+    cin >> nBoys;
+    for(i = 0; i < nBoys; ++i)
+        cin >> boy[i];
+ 
+    cin >> nGirls;
+    for(i = 0; i < nGirls; ++i)
+        cin >> girl[i];
+ 
+    sort(boy, boy+nBoys);
+    sort(girl, girl+nGirls);
+ 
+    i = j = res = 0;
+    while(i < nBoys && j < nGirls)
+    {
+        if(abs(boy[i]-girl[j]) <= 1)
+        {
+            ++res;
+            ++i;
+            ++j;
+        }
+        else if(boy[i] < girl[j])
+            ++i;
+        else
+            ++j;
+    }
+ 
+    cout << res << '\n';
+ 
+    return 0;
+}
