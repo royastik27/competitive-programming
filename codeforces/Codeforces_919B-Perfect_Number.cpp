c8683787a88ab2fb5b05afff2d44cbe9cc3bbc25
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-05-07 20:28:13 +0600

    Create Codeforces_919B-Perfect_Number.cpp

---
+++ codeforces/Codeforces_919B-Perfect_Number.cpp
@@ -0,0 +1,47 @@
+/**
+* Codeforces Round #460 (Div. 2)
+* Problem B - Perfect Number
+* Time: 30 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+
+using namespace std;
+
+#define LIMIT 10000
+
+bool isSum10(int n)
+{
+    int sum = 0;
+
+    while(n)
+    {
+        sum += n%10;
+        n /= 10;
+    }
+
+    return (sum == 10) ? true : false;
+}
+
+int main()
+{
+    int k, i, num;
+
+    cin >> k;
+
+    i = 0;
+    num = 10;
+
+    while(i < k)
+    {
+        num += 9;
+
+        if(isSum10(num))
+            ++i;
+    }
+
+    cout << num << '\n';
+
+    return 0;
+}
