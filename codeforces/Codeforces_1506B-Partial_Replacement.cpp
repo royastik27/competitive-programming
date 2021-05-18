2b813fa750444a4ed426ba261d5e1c9efec85067
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-05-18 22:04:37 +0600

    Create Codeforces_1506B-Partial_Replacement.cpp

---
+++ codeforces/Codeforces_1506B-Partial_Replacement.cpp
@@ -0,0 +1,49 @@
+/**
+* Codeforces Round #710 (Div. 3)
+* Problem B - Partial Replacement
+* Time: 15 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int TC, star[50], j, len, k, op, last;
+    char str[51];
+    register int i;
+
+    cin >> TC;
+
+    while(TC--)
+    {
+        cin >> len >> k >> str;
+
+        for(i = 0, j = 0; str[i] != '\0'; ++i)
+        {
+            if(str[i] == '*')
+                star[j++] = i;
+        }
+
+        op = 0;
+        for(i = star[0]; i <= star[j-1]; i += k)
+        {
+            i = *(upper_bound(star, star+j, i) - 1);
+            last = i;
+            ++op;
+        }
+
+        if(last != star[j-1])
+            ++op;
+
+        cout << op << '\n';
+    }
+
+    return 0;
+}
