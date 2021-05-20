e90994ea8229cf7f262039850f4dd2599faf6404
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-05-20 23:44:53 +0600

    Create Codeforces_1490C-Sum_of_Cubes.cpp

---
+++ codeforces/Codeforces_1490C-Sum_of_Cubes.cpp
@@ -0,0 +1,67 @@
+/**
+* Codeforces Round #702 (Div. 3)
+* Problem C - Sum of Cubes
+* Time: 46 ms
+* AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define LIM 100000
+
+vector <long long int> num;
+
+void generate_num()
+{
+    long long int i;
+    for(i = 1; i <= LIM; ++i)
+        num.push_back(i*i*i);
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    generate_num();
+
+    int TC, low, high;
+    long long int x, sum;
+    bool is_representable;
+
+    cin >> TC;
+
+    while(TC--)
+    {
+        cin >> x;
+
+        low = 0;
+        high = LIM - 1;
+
+        is_representable = false;
+
+        while(low <= high)
+        {
+            sum = num[low] + num[high];
+
+            if(sum == x) {
+                is_representable = true;
+                break;
+            }
+            else if(sum > x)
+                --high;
+            else
+                ++low;
+        }
+
+        if(is_representable) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
