91a1a2685df0b058a1d69f2c7aa271361af3f1ee
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-05-29 18:09:01 +0600

    Update Codeforces_208A-Dubstep.cpp

--- codeforces/Codeforces_208A-Dubstep.cpp
+++ codeforces/Codeforces_208A-Dubstep.cpp
@@ -1,31 +1,45 @@
+/**
+* Codeforces Round #130 (Div. 2)
+* Problem A - Dubstep
+* Time: 62 ms
+* AUTHOR: Astik Roy
+**/
+
 #include <iostream>
 #include <cstring>
- 
+
 using namespace std;
- 
+
+char str[201];
+
+inline bool isWUB(int i, int len)
+{
+    return (len - i > 2 && str[i] == 'W' && str[i+1] == 'U' && str[i+2] == 'B');
+}
+
 int main()
 {
-    char str[201], res[201];
+    char res[201];
     int i, j, len;
- 
+
     cin >> str;
- 
+
     len = strlen(str);
- 
+
     i = 0;
     j = -1;
- 
+
     // determining the first character
-    if(len - i > 2 && str[i] == 'W' && str[i+1] == 'U' && str[i+2] == 'B') {
-        while(len - i > 2 && str[i] == 'W' && str[i+1] == 'U' && str[i+2] == 'B')
+    if(isWUB(i, len)) {
+        while(isWUB(i, len))
             i += 3;
- 
+
         res[++j] = str[i++];
     }
- 
+
     while(i < len)
     {
-        if(len - i > 2 && str[i] == 'W' && str[i+1] == 'U' && str[i+2] == 'B') {
+        if(isWUB(i, len)) {
             if(res[j] != ' ') // for avoiding extra space
                 res[++j] = ' ';
             i += 3;
@@ -34,13 +48,13 @@ int main()
             res[++j] = str[i++];
         }
     }
- 
+
     // terminating string
     if(res[j] == ' ')
         res[j] = '\0';
     else res[++j] = '\0';
- 
+
     cout << res << '\n';
- 
+
     return 0;
 }
