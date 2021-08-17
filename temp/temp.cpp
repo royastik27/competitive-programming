531ba24a70e3b5939e362127bce043eb8887f2fc
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-08-17 22:26:09 +0600

    Create temp.cpp

---
+++ temp/temp.cpp
@@ -0,0 +1,45 @@
+/**
+ * 
+ * Problem 
+ * Time: 
+ * AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define LIM 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    
+    int n, total, pre[LIM], el, res;
+    vector <int> time;
+    vector <int>::iterator it;
+    register int i;
+
+    cin >> n >> total;
+
+    cin >> el;
+    time.push_back(el);
+    pre[0] = time[0];
+
+    for(i = 1; i < n; ++i) {
+        cin >> el;
+        time.push_back(el);
+        pre[i] = pre[i-1] + time[i];
+    }
+
+    res = 0;
+    for(it = time.begin(); it != time.end(); ++it) {
+        res = max((int)distance(it, upper_bound(time.begin(), time.end(), pre[i]+total)), res);
+    }
+
+    cout << res << '\n';
+
+    return 0;
+}
