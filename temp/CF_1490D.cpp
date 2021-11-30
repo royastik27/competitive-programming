f7676df5814d7eee2e382f7f76824744a142bbbe
Astik Roy <76438969+royastik27@users.noreply.github.com>
2021-11-30 09:54:25 +0600

    Create CF_1490D.cpp

---
+++ temp/CF_1490D.cpp
@@ -0,0 +1,55 @@
+/**
+ *
+ * Problem
+ * Time:
+ * AUTHOR: Astik Roy
+**/
+
+#include <iostream>
+
+#define LIM 100
+
+using namespace std;
+
+int ara[LIM], dep[LIM], n;
+bool vis[LIM];
+
+void solve(int b, int e, int level)
+{
+    int mx = 0, mx_pos = -1, i;
+
+    for(i = b; i <= e; ++i) {
+        if(!vis[i] && ara[i] > mx) mx = ara[i], mx_pos = i;
+    }
+
+    if(mx) {
+        vis[mx_pos] = true;
+        deg[mx_pos] = level;
+        solve(0, mx_pos-1, level+1);
+        solve(mx_pos+1, n, level+1);
+    }
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    //cin.tie(NULL);
+
+    int TC;
+
+    cin >> TC;
+
+    while(TC--) {
+        cin >> n;
+        --n;
+
+        memset(vis, 0, n*sizeof(bool));
+
+        solve();
+    }
+
+
+    return 0;
+}
