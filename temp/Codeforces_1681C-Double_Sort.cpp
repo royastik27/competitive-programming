03651ae9e0700af20916a5a02f7ba95c1bd25397
Astik Roy <76438969+royastik27@users.noreply.github.com>
2022-06-17 12:48:26 +0600

    Update Codeforces_1681C-Double_Sort.cpp

--- temp/Codeforces_1681C-Double_Sort.cpp
+++ temp/Codeforces_1681C-Double_Sort.cpp
@@ -9,6 +9,8 @@
 #include <cstring>
 #include <bitset>
 #include <algorithm>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
@@ -21,13 +23,38 @@ bool checkBit(int n, int pos)
 
 void setBit(int * n, int pos) { *n |= (1 << pos); }
 
+void printPosition(int ara[], int n)
+{
+    int i, j;
+
+    cout << "positions:\n";
+    for(i = 1; i <= n; ++i)
+    {
+        cout << i << ": ";
+        for(j = 1; j <= n; ++j)
+        {
+            if(checkBit(ara[i], j)) cout << j << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+void swap(int * a, int * b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+    return;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     // cin.tie(NULL);
 
-    int a[LIM], sortedA[LIM], b[LIM], sortedB[LIM], TC, n, i, pos[LIM];
+    int a[LIM], sortedA[LIM], b[LIM], sortedB[LIM], TC, n, i, j, posA[LIM], posB[LIM];
     bool impossible;
+    vector <pair <int, int> > swaps;
 
     cin >> TC;
 
@@ -35,20 +62,21 @@ int main()
     {
         cin >> n;
 
-        memset(pos, 0, (n+1)*sizeof(int));
+        memset(posA, 0, (n+1)*sizeof(int));
+        memset(posB, 0, (n+1)*sizeof(int));
 
         for(i = 1; i <= n; ++i)
         {
             cin >> a[i];
             sortedA[i] = a[i];
-            setBit(&pos[a[i]], i);
+            setBit(&posA[a[i]], i);
         }
 
         for(i = 1; i <= n; ++i)
         {
             cin >> b[i];
             sortedB[i] = b[i];
-            setBit(&pos[b[i]], i);
+            setBit(&posB[b[i]], i);
         }
 
         // SORTING
@@ -57,22 +85,70 @@ int main()
 
         // MAIN OPERATION
         impossible = false;
-        for(i = 1; i <= n; ++i)
+
+        for(i = 1; i <= n - 1; ++i)
         {
-            //
+            if(a[i] == sortedA[i])
+            {
+                if(b[i] == sortedB[i])
+                    continue;
+                else
+                {
+                    // check other positions of a[i] that match with any position of sortedB[i]
+                    for(j = i + 1; j <= n; ++j)
+                    {
+                        if(checkBit(posA[a[i]], j) && checkBit(posB[sortedB[i]], j))
+                        {
+                            // update positions
+                            // swapping
+                            swap(&a[i], &a[j]);
+                            swap(&b[i], &b[j]);
+                            // updating answers
+                            swaps.push_back(i, j);
+                            break;
+                        }
+                    }
+
+                    if(j > n)
+                    {
+                        // not found
+                        impossible = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for(j = i + 1; j <= n; ++j)
+                {
+                    if(checkBit(posA[a[i]], j) && checkBit(posB[sortedB[i]], j))
+                    {
+                        // update positions
+                        // swapping
+                        swap(&a[i], &a[j]);
+                        swap(&b[i], &b[j]);
+                        // updating answers
+                        swaps.push_back(i, j);
+                        break;
+                    }
+                }
+                // if b is sorted
+                // if not sorted
+            }
         }
 
-        // cout << "positions:\n";
-        // for(i = 1; i <= n; ++i)
-        // {
-        //     cout << i << ": ";
-        //     for(int j = 1; j <= n; ++j)
-        //     {
-        //         if(checkBit(pos[i], j)) cout << j << ' ';
-        //     }
-        //     cout << '\n';
-        // }
+        if(impossible) cout << -1 << '\n';
+        else
+        {
+            cout << swaps.size() << '\n';
+            for(i = 0; i < swaps.size(); ++i)
+                cout << swaps[i].first << ' ' << swaps[i].second << '\n';
+            swaps.clear();
+        }
     }
+
+    return 0;
+}
 
     return 0;
 }
