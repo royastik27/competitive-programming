ab7b3b2918f020b69bfe402df53506f08e074d65
Astik Roy <76438969+royastik27@users.noreply.github.com>
2022-06-21 01:28:44 +0600

    Update Codeforces_1697C-awoo's_Favorite_Problem.cpp

--- temp/Codeforces_1697C-awoo's_Favorite_Problem.cpp
+++ temp/Codeforces_1697C-awoo's_Favorite_Problem.cpp
@@ -5,6 +5,13 @@
 * AUTHOR: Astik Roy
 **/
 
+/**
+* Educational Codeforces Round 130 (Rated for Div. 2)
+* Problem C - awoo's Favorite Problem
+* Time: 
+* AUTHOR: Astik Roy
+**/
+
 #include <iostream>
 #include <algorithm>
 
@@ -15,10 +22,11 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
+    cin.tie(NULL);
 
-    int TC, n, ptr[LIM], ptrB, ptrC, i;
+    int TC, n, ptr[LIM], ptrB, ptrC, i, idx;
     char str[LIM + 1], ans[LIM + 1];
+    bool impossible;
 
     cin >> TC;
 
@@ -27,7 +35,7 @@ int main()
         cin >> n >> str >> ans;
 
         // PREPROCESSING
-        ptrB = ptrC = 0;
+        ptrB = ptrC = impossible = 0;
 
         for(i = n - 1; i >= 0; --i)
         {
@@ -48,10 +56,36 @@ int main()
             }
         }
 
-        cout << "Preprocessed Result:\n";
+        // CHECKING
         for(i = 0; i < n; ++i)
-            cout << ptr[i] << ' ';
-        cout << '\n';
+        {
+            if(str[i] == ans[i])
+                continue;
+            else if(str[i]+1 == ans[i] && ptr[i])
+            {
+                idx = ptr[i];
+
+                if(str[idx] == ans[i])
+                {
+                    str[idx] = str[i];
+                    
+                    if(str[idx] == str[idx+1])
+                        ptr[idx] = ptr[idx+1];
+                    else if(str[idx]+1 == str[idx+1])
+                        ptr[idx] = idx+1;
+                    else ptr[idx] = 0;
+                }
+                else { impossible = true; break; }
+            }
+            else
+            {
+                impossible = true;
+                break;
+            }
+        }
+
+        if(impossible) cout << "NO\n";
+        else cout << "YES\n";
     }
 
     return 0;
