dc438a94749fb4f160c5954e1d2b3b4757a3cc00
royastik27 <royastik27@gmail.com>
2022-06-30 14:12:25 +0600

    Cloning to local

---
+++ temp/Codeforces_1167B.cpp
@@ -0,0 +1,18 @@
+/**
+ * Educational Codeforces Round 65 (Rated for Div. 2)
+ * Lost Numbers
+ * URL: https://codeforces.com/problemset/problem/1167/B
+**/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    // CODE HERE
+
+    return 0;
+}
