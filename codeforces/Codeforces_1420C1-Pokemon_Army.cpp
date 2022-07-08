95c2dd5aac2a7ca2e5b984a7657697db3406423b
royastik27 <royastik27@gmail.com>
2022-07-09 01:05:53 +0600

    Solved a mistake

--- codeforces/Codeforces_1420C1-Pokemon_Army.cpp
+++ codeforces/Codeforces_1420C1-Pokemon_Army.cpp
@@ -9,25 +9,55 @@
 
 using namespace std;
 
+#define LIM 300001
+
 class Solution
 {
-    public:
-    void solve()
-    {
-        // CODE HERE
+    int arr[LIM];
 
-        return;
+    public:
+    long long int solve()
+    {
+        long long int ans = 0;
+        int n, nQueries, i, add, mn;
+
+        cin >> n >> nQueries;
+
+        for(i = 0; i < n; ++i) cin >> arr[i];
+
+        mn = 0; add = arr[0];
+
+        for(i = 1; i < n; ++i)
+        {
+            if(arr[i] < arr[i-1])
+            {
+                ans += add;
+                mn = arr[i];
+                add = 0;
+            }
+            else
+                add = arr[i] - mn;
+        }
+
+        ans += add;
+        
+        return ans;
     }
 };
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
+    cin.tie(NULL);
 
     Solution sol;
 
-    // DRIVER CODE
+    int TC;
+
+    cin >> TC;
+
+    while(TC--)
+        cout << sol.solve() << '\n';
     
     return 0;
 }
--- temp/main.cpp
+++ temp/main.cpp
@@ -9,55 +9,25 @@
 
 using namespace std;
 
-#define LIM 300001
-
 class Solution
 {
-    int arr[LIM];
-
     public:
-    long long int solve()
+    void solve()
     {
-        long long int ans = 0;
-        int n, nQueries, i, add, mn;
-
-        cin >> n >> nQueries;
-
-        for(i = 0; i < n; ++i) cin >> arr[i];
-
-        mn = 0; add = arr[0];
-
-        for(i = 1; i < n; ++i)
-        {
-            if(arr[i] < arr[i-1])
-            {
-                ans += add;
-                mn = arr[i];
-                add = 0;
-            }
-            else
-                add = arr[i] - mn;
-        }
-
-        ans += add;
+        // CODE HERE
         
-        return ans;
+        return;
     }
 };
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    // cin.tie(NULL);
 
     Solution sol;
 
-    int TC;
-
-    cin >> TC;
-
-    while(TC--)
-        cout << sol.solve() << '\n';
+    // DRIVER CODE
     
     return 0;
 }
