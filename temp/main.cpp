0201394e51b7e310b5352a87d76075a9ddf879d3
royastik27 <royastik27@gmail.com>
2022-10-13 21:43:09 +0600

    Whatever...

--- temp/main.cpp
+++ temp/main.cpp
@@ -6,17 +6,38 @@
 **/
 
 #include <iostream>
+#include <map>
  
 using namespace std;
- 
-#define LIM 
+
+#define LIM 200001
  
 class Solution {
-    
+    int arr[LIM], que[LIM];
+    long long pre[LIM];
+    map <int, int> mp;
 public:
     void solve()
     {
-        cout << "HELLO\n";
+        int n, q, i;
+
+        cin >> n >> q;
+
+        arr[0] = pre[0] = 0;
+
+        for(i = 1; i <= n; ++i)
+        {
+            cin >> arr[i];
+            pre[i] = pre[i-1] + arr[i];
+        }
+
+        for(i = 0; i < q; ++i)
+        {
+            cin >> que[i];
+            mp
+        }
+
+        mp.clear();
         
         return;
     }
