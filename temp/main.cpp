19287202dbfc186865fb5ce88824574df8440c06
Astik Roy <royastik27@gmail.com>
2023-06-06 21:51:16 +0600

    Contest Time

--- temp/main.cpp
+++ temp/main.cpp
@@ -6,72 +6,80 @@
 **/
 
 #include <iostream>
+#include <cmath>
+#include <set>
+#include <vector>
+#include <utility>
 #include <algorithm>
-#include <cstring>
 
 using namespace std;
 
 typedef long long int ll;
-
-#define LIM 1000
+typedef pair <int, int> pii;
 
 class Solution
 {
-    int arr[LIM];
-    ll ans[LIM];
+    set <int> st;
+    set <int>::iterator it;
+    vector <int> vec;
+
+    int go(int b, int e)
+    {
+        int avg = (vec[b] + vec[e]) / 2;
+
+        int ans = 0;
+        for(int i = b; i <= e; ++i)
+            ans = max(ans, abs(vec[i]-avg));
+
+        return ans;
+    }
 public:
     void solve()
     {
-        int n, i, grp, cnt, j, idx;
-        ll sum;
+        int n, i, num;
 
         cin >> n;
 
         for(i = 0; i < n; ++i)
-            cin >> arr[i];
-
-        sort(arr, arr+n);
-
-        cin >> grp >> sum;
-
-        if(n % grp)
         {
-            cout << "NO\n";
-            return;
+            cin >> num;
+            st.insert(num);
         }
 
-        cnt = n / grp;
+        for(it = st.begin(); it != st.end(); ++it)
+            vec.push_back(*it);
 
-        memset(ans, 0, sizeof(ans));
-        idx = -1;
+        n = vec.length();
 
-        bool front = true;
-        for(i = 0; i < cnt; ++i)
+        if(n < 4)
         {
-            if(front)
-            {
-                for(j = 0; j < grp; ++j)
-                    ans[j] += arr[++idx];
-            }
-            else
-            {
-                for(j = grp - 1; j >= 0; --j)
-                    ans[j] += arr[++idx];
-            }            
+            ans = 0;
+        }
+        else
+        {
+            vector <pii> temp;
 
-            front ^= 1;       
+            for(i = 1; i < n; ++i)
+                temp.push_back({ vec[i] - vec[i-1], i });
+
+            sort(temp.begin(), temp.end(), greater <pii>);
+
+            vector <int> pos;
+            pos.push_back(temp[0].second);
+            pos.push_back(temp[1].second);
+
+            sort(pos.begin(), pos.end());
+
+            ans = 0;
+            ans = max(ans, go(0, pos[0]-1));
+            ans = max(ans, go(pos[0], pos[1]-1));
+            ans = max(ans, go(pos[1], n-1));
         }
 
-        for(i = 1; i < grp; ++i)
-        {
-            if(ans[i] != ans[0])
-            {
-                cout << "NO\n";
-                return;
-            }
-        }
+        cout << ans << '\n';
 
-        cout << "YES\n";
+        st.clear();
+        vec.clear();
 
         return;
     }
@@ -83,8 +91,12 @@ int main()
     // cin.tie(NULL);
 
     Solution sol;
-    
-    sol.solve();
+    int TC;
+
+    cin >> TC;
+
+    while(TC--)
+        sol.solve();
 
     return 0;
 }
