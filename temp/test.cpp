9bae3cb9a06bf39987df4e9ff5c538e835419663
Astik Roy <royastik27@gmail.com>
2023-10-10 02:22:31 +0600

    After a long time...

--- temp/test.cpp
+++ temp/test.cpp
@@ -1,36 +1,108 @@
-/**
-* [name]
-* Problem 
-* TIME: 
-* AUTHOR: Astik Roy
-**/
-
-#include <iostream>
-
+MH Utsho
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-typedef long long int ll;
 
-class Solution
-{
 
-public:
-    void solve()
-    {
-        // 
+void prime(int a);
 
-        return;
-    }
-};
+bool check_palindromic(int a);
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
+    int start,end;
 
-    Solution sol;
+    cin>>start>>end;
+
+    for (int i = start; i <=end; i++)
+    {
+        
+        prime(i); 
+    }
     
-    sol.solve();
 
     return 0;
+}
+
+void prime(int a){
+
+    bool TorF=true;
+    for (int i = 2; i <=sqrt(a); i++)
+    {
+        if(a%i==0 && a!=1){
+            TorF=false;
+            break;
+        }
+    }
+
+    if(TorF && a!=1){
+
+        if(check_palindromic(a)){
+            cout<<a<<endl;
+        }
+        
+
+    }
+    else return;
+    
+}
+
+bool check_palindromic(int a){
+
+    if(a>=10 && a<100){
+
+        int second=a%10;
+        int first=a/10;
+
+        if(first==second) return true;
+
+        else return false;
+        
+
+    }
+
+    if(a>=100 && a<1000){
+
+        int third=a%10;
+        a=a/10;
+        int first =a/10;
+
+        if(first==third) return true;
+
+        else return false;
+
+    }
+
+    if(a>=1000 && a<10000){
+
+        int forth=a%10;
+           a=a/10;
+        int third=a%10;
+           a=a/10; 
+        int second=a%10;
+        int first=a/10;
+
+        if(first==forth && second==third ) return true;
+
+        else return false;
+
+    }
+
+    if(a>=10000 && a<100000){
+      
+       int fifth=a%10;
+        a=a/10;
+       int forth=a%10;
+           a=a/10;
+       int third=a%10;
+           a=a/10; 
+       int second=a%10;
+       int first=a/10; 
+
+        if(first==fifth && second==forth ) return true;
+
+        else return false;
+
+    }
 }
