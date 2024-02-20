1e1bee929f5c4b5bbb78c955b0bb0fd6b994f2a9
Astik Roy <royastik27@gmail.com>
2024-02-20 18:22:27 +0600

    Contest time

--- temp/main.cpp
+++ temp/main.cpp
@@ -20,15 +20,17 @@ typedef pair <int, int> pii;
 // typedef tree<int , null_mapped_type ,  less<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_set;
 // typedef tree<int , null_mapped_type ,  less_equal<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_multiset;
 
+int days[14] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 class Solution
 {
-
+    map <int> mp[7];
 public:
     void solve()
     {
         int i;
         string inp;
-        
+
         getline(cin, inp);
 
         int year = 0;
@@ -39,6 +41,17 @@ public:
         bool leapYear = false;
         if(year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))
             leapYear = true;
+
+        if(leapYear)
+            days[2] = 29;
+        else days[2] = 28;
+
+        int month = 0;
+        month = month * 10 + int(dateStr[3]-'0');
+        month = month * 10 + int(dateStr[4]-'0');
+
+
+
 
         return;
     }
