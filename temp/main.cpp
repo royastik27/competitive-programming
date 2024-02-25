6d93d58af2130645672f68dfa972b0521c430a2c
Astik Roy <royastik27@gmail.com>
2024-02-25 11:10:38 +0600

    Contest time

--- temp/main.cpp
+++ temp/main.cpp
@@ -6,6 +6,7 @@
 **/
 
 #include <iostream>
+#include <cstring>
 
 // #include <ext/pb_ds/assoc_container.hpp>
 // #include <ext/pb_ds/tree_policy.hpp>
@@ -20,8 +21,16 @@ typedef pair <int, int> pii;
 // typedef tree<int , null_mapped_type ,  less<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_set;
 // typedef tree<int , null_mapped_type ,  less_equal<int> , rb_tree_tag , tree_order_statistics_node_update> ordered_multiset;
 
+#define LIM 1000
+
+int row_move[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int col_move[8] = { -1, 0, 1, -1, 1, -1, 0, 1 }
+
 class Solution
 {
+    int n, m, a[LIM][LIM], mem[LIM][LIM];
+    ll ans;
+
     bool check(int i, int j, char ch)
     {
         if(i < 0 || i == n || j < 0 || j == m)
@@ -29,16 +38,25 @@ class Solution
 
         return (a[i][j] == ch);
     }
+
+    int check2(int i, int j)
+    {
+        if(i < 0 || i == n || j < 0 || j == m)
+            return 0;
+
+        return mem[i][j];
+    }
+
     void first()
     {
-        int i, j;
+        int i, j, x, y, k;
 
         for(i = 0; i < n; ++i) {
             for(j = 0; j < m; ++j) {
 
                 if(a[i][j] == inp[1]) {
 
-                    for(k = 0; k < 4; ++k) {
+                    for(k = 0; k < 8; ++k) {
                         x = i + row_move[k];
                         y = j + col_move[k];
 
@@ -47,6 +65,31 @@ class Solution
                     }
                 }
             }
+        }
+
+        return;
+    }
+
+    void second()
+    {
+        int i, j, res, x, y, k;
+
+        for(i = 0; i < n; ++i) {
+            for(j = 0; j < m; ++j) {
+
+                if(a[i][j] == inp[0]) {
+                    res = 0;
+
+                    for(k = 0; k < 8; ++k) {
+                        x = i + row_move[k];
+                        y = j + col_move[k];
+
+                        res += check2(x, y);
+                    }
+                }
+            }
+
+            ans = max(ans, res);
         }
     }
 
@@ -68,6 +111,11 @@ public:
         cin >> inp;
 
         first();
+
+        ans = 0;
+        second();
+
+        cout << ans << '\n';
 
         return;
     }
