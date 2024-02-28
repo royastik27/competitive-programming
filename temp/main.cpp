c2e685b2ea3135779760e1ac3678dee273574789
Astik Roy <royastik27@gmail.com>
2024-02-28 09:53:06 +0600

    Whatever...

--- temp/main.cpp
+++ temp/main.cpp
@@ -25,12 +25,38 @@ typedef pair <int, int> pii;
 class Solution
 {
     vector <ll> p;
+    vector <ll>::iterator LB;
+
+    ll check(ll tot, ll u)
+    {
+            if(tot <= u) {
+                    int cnt = u - tot;
+
+                    ll right = ll(u+1)*u / 2;
+                    ll left = ll(cnt+1)*cnt / 2;
+
+                    return (right - left); 
+                }
+                else {
+                    // tot > u
+                    int cnt = (u+1) - tot;
+
+                    ll right = ll(u+1)*u / 2;
+                    ll left = ll(cnt+1)*cnt / 2;
+
+                    return (right - left); 
+                }
+    }
 public:
     void solve()
     {
-        ll tot;
+        int n, i, ai, q, l;
+        ll u;
+        // ll tot;
 
         cin >> n;
+
+        p.resize(n+1);
 
         p[0] = 0;
         for(i = 1; i <= n; ++i) {
@@ -47,41 +73,58 @@ public:
 
             LB = lower_bound(p.begin(), p.end(), u);
 
-            if(*LB == u || *LB == u+1) {
+            if(LB != p.end() && (*LB == u || *LB == u+1)) {
                 int r = distance(p.begin(), LB);
 
-                cout << r << '\n';
+                cout << r << ' ';
             }
             else {
-                r = distance(p.begin(), LB);
+                int r = distance(p.begin(), LB);
 
                 if(l == r) {
-                    tot = p[r] - p[r-1];
+                    cout << r << ' ';
                 }
                 else {
-                    --r;
-                    tot = p[r] - p[l-1];
+                    // check r and r-1;
+                    ll tot_r = p[r] - p[l-1];
+                    ll tot_r1 = p[r-1] - p[l-1];
+
+                    if(r > n)
+                        cout << (r-1) << ' ';
+                    else  {
+                        ll inc_r = check(tot_r, u);
+                        ll inc_r1 = check(tot_r1, u);
+
+                        if(inc_r1 == inc_r)
+                            cout << (r-1) << ' ';
+                        else if(inc_r1 >= inc_r)
+                            cout << (r-1) << ' ';
+                        else
+                            cout << (r) << ' ';
+                    }
+                    
                 }
 
-                if(tot <= u) {
-                    int cnt = u - tot;
+                // cout << "LB: " << r << '\n';
 
-                    ll right = ll(u+1)*u / 2;
-                    ll left = ll(cnt+1)*cnt / 2;
+                // if(l != r)
+                //     --r;
 
-                    cout << (right - left) << '\n'; 
-                }
-                else {
-                    // tot > u
-                    int cnt = (u+1) - tot;
+                // cout << "HERE\n";
 
-                    ll right = ll(u+1)*u / 2;
-                    ll left = ll(cnt+1)*cnt / 2;
+                // cout << r << ' ';
 
-                    cout << (right - left) << '\n'; 
-                }
+                // if(l == r) {
+                //     tot = p[r] - p[r-1];
+                // }
+                // else {
+                //     --r;
+                //     // tot = p[r] - p[l-1];
+                // }
             }
         }
+
+        cout << '\n';
 
         return;
     }
